4852cfe36635eef3e1fff9c9684a5dcbdc377316
mrsix <1658530280@qq.ocm>
2020-03-13 22:12:45 +0800

    添加滑动条

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -14,6 +14,7 @@ import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,46 +22,62 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     final String tag = "Sensor";
     SensorManager sm = null;
-    TextView View1 = null;
-    TextView View2 = null;
-    TextView View3 = null;
-    TextView View4 = null;
-    TextView View5 = null;
+    TextView ShowIP = null;
+    TextView ShowSensor = null;
+    private TextView showspeed = null;
     private TextView tv_content, tv_send_text;
     private Button bt_send;
     private Button bt_clean;
     String ShowInfomation = "";
-
+    SeekBar ControlSpeed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sm = (SensorManager) getSystemService(SENSOR_SERVICE);
         /*输出框初始化*/
-        View1 = (TextView) findViewById(R.id.edt1);
-        View2 = (TextView) findViewById(R.id.edt2);
-        View3 = (TextView) findViewById(R.id.edt3);
-        View4 = (TextView) findViewById(R.id.edt4);
-        View5 = (TextView) findViewById(R.id.edt5);
+        ShowIP          = findViewById(R.id.ShowIP);
+        ShowSensor      = findViewById(R.id.ShowSensor);
+        tv_content      = findViewById(R.id.tv_content);
+        tv_send_text    = findViewById(R.id.tv_send_text);
+        showspeed       = findViewById(R.id.ShowSpeed);
+        /*按钮初始化*/
+        bt_send     = findViewById(R.id.bt_send);
+        bt_clean    = findViewById(R.id.bt_clean);
+        bt_send.setOnClickListener(clickListener);
+        bt_clean.setOnClickListener(clickListener);
+        /*滚动条初始化*/
+        ControlSpeed = findViewById(R.id.ControlSpeed);
+        ControlSpeed.setOnSeekBarChangeListener(seekBarChangeListener);
         /*获取IP相关*/
         Context mContext = getApplicationContext();
-        View1.setText("IP：" + GetLocalIP.getLocalIPAddress(mContext));
+        ShowIP.setText("IP：" + GetLocalIP.getLocalIPAddress(mContext));
         /*网络连接相关*/
-        InitView();
         MobileServer mobileServer = new MobileServer();
         mobileServer.setHandler(handler);
         new Thread(mobileServer).start();
     }
+    private SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener(){
+        /*改变时触发*/
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            if(seekBar.getId() == R.id.ControlSpeed){
+                showspeed.setText("Speed:" + progress);
+            }
+        }
 
-    private void InitView() {
-        tv_content = (TextView) findViewById(R.id.tv_content);
-        tv_send_text = (TextView) findViewById(R.id.tv_send_text);
-        bt_send = (Button) findViewById(R.id.bt_send);
-        bt_clean = (Button) findViewById(R.id.bt_clean);
-        bt_send.setOnClickListener(clickListener);
-        bt_clean.setOnClickListener(clickListener);
-    }
+        /*按住触发*/
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
 
+        }
+        /*放开触发*/
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+
+    };
     private View.OnClickListener clickListener = new View.OnClickListener(){
         @Override
         public void onClick(View v) {
@@ -87,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
             synchronized (this) {
                 String str = "X：" + event.values[0] + "，\nY：" + event.values[1] + "，\nZ：" + event.values[2];
                 if (event.sensor.getType() == Sensor.TYPE_ORIENTATION) {       // 方向传感器信息
-                    View3.setText("方向传感器：\n" + str);
+                    ShowSensor.setText("方向传感器：\n" + str);
                 }
             }
         }
